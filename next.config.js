5104b2208654842f5a801d531c0a7160e89b4ed4
sea <boywinterfull@gmail.com>
2023-10-26 21:03:51 +0700

    5

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  
 }
+
+module.exports = {
+  // ตัวเลือกการกำหนดค่าอื่น ๆ...
+  env: {
+    NEXTAUTH_SECRET: 'https://project-sea.vercel.app/', // แทนที่ด้วยคีย์ลับจริงของคุณ
+  },
+};
 
 module.exports = nextConfig
